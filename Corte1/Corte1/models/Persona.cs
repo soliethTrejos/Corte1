0730fba46ac5ea5022dd2641fc79ed4f18deb909
solieth <soliethv@uamv.edu.ni>
2024-09-23 08:37:34 -0600

    Persona

---
+++ Corte1/Corte1/models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    internal class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Ciudad { get; set; }
+
+        public Persona(string nombres, string apellidos, DateTime fechaNacimiento, string ciudad)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+            FechaNacimiento = fechaNacimiento;
+            Ciudad = ciudad;
+        }
+    }
+}
+  
+
